2d61e134cc8fcf130f7e44a1367f7de13c442c00
Leoncio1337 <leonardo.k@escolar.ifrn.edu.br>
2024-02-26 08:17:45 -0300

    "novas operações como add, multi, divi e sub"

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,3 +1,14 @@
-function mudarTexto(x) {
-    document.getElementById(x).innerHTML = 'mudou'
-}
+const x = parseInt(prompt('informe o primeiro numero'));
+const y = parseInt(prompt('informe o segundo numero'));
+function soma(x, y) {
+    return alert(x + y)
+};
+function subtracao(x, y) {
+    return alert(x - y)
+};
+function divisao(x, y) {
+    return alert(x / y)
+};
+function multiplicacao(x, y) {
+    return alert(x * y)
+};
