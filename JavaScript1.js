8b280113e3e40cd11c3845d1d76c1c5d1947b0f2
Leoncio1337 <leonardo.k@escolar.ifrn.edu.br>
2024-03-04 07:52:37 -0300

    CALCULO DE IMC, PRIMEIRO EXERCÍCIO EM SALA DE AULA

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,23 +1,21 @@
-var x;
-var y;
-console.log(x)
-console.log(y)
-function submissao() {
-    x = parseInt(document.getElementById('numero1').value);
-    y = parseInt(document.getElementById('numero2').value);
-    console.log(x)
-    console.log(y)
-}
+function submeter() {
+    let m = parseFloat(document.getElementById("massa").value)
+    let n = parseFloat(document.getElementById("altura").value)
+    let imc = m / (n*n);
+    if (imc < 18.5) {
+        alert("CLASSIFICAÇÃO: MAGREZA, SEU IMC É " + imc)
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        alert("CLASSIFICAÇÃO: NORMAL, SEU IMC É " + imc)
+    }
+    else if (imc > 24.9 && imc <= 29.9) {
+        alert("CLASSIFICAÇÃO: SOBREPESO, SEU IMC É " + imc)
+    }
+    else if (imc > 29.9 && imc <= 39.9) {
+        alert("CLASSIFICAÇÃO: OBESIDADE, SEU IMC É " + imc)
+    }
+    else if (imc > 39.9) {
+        alert("CLASSIFICAÇÃO: OBESIDADE GRAVE, SEU IMC É " + imc)
+    }
 
-function soma(x, y) {
-    return alert(x + y)
-};
-function subtracao(x, y) {
-    return alert(x - y)
-};
-function divisao(x, y) {
-    return alert(x / y)
-};
-function multiplicacao(x, y) {
-    return alert(x * y)
-};
+}
