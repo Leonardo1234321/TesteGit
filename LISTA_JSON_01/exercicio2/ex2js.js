7c6d958514046b33b1d8f88f06f5c6e9b1b32b75
Leoncio1337 <leonardo.k@escolar.ifrn.edu.br>
2024-08-13 15:21:16 -0300

    continuação do exercicio2 de JSON

--- LISTA_JSON_01/exercicio2/ex2js.js
+++ LISTA_JSON_01/exercicio2/ex2js.js
@@ -3,21 +3,27 @@ fetch("https://jsonplaceholder.typicode.com/users")
 .then(users => {
         fetch("https://jsonplaceholder.typicode.com/posts").then(posts => posts.json())
         .then(data => {
-            data.forEach(post => {
-                let user_name = users[post["userId"] - 1]["name"];
-                let title = post["title"];
-                let body = post["body"];
-                let postID = post["id"]
-                displayPosts(user_name, title, body, postID);
-            }
+            var comments_string;
+            await fetch(`https://jsonplaceholder.typicode.com/posts/${data[0]["id"]}/comments`)
+            .then(comments => comments.json())
+            .catch(error => displayError(error))
+            .then(blob => comments_string = blob)
+            let user_name = users[data[0]["userId"] - 1]["name"];
+            let title = data[0]["title"];
+            let body = data[0]["body"];
+            var postID = data[0]["id"]
+            displayPosts(user_name, title, body, postID);
+        }
             )
         })
         .catch(error => displayError(error))
-    }   
-)
 .catch(error => displayError(error))
 
-fetch ()
+fetch ("https://jsonplaceholder.typicode.com/comments")
+.then(response => responde.json())
+.then(comments => {
+    let id_posts = await postID
+})
 function displayError(error) {
     const userList = document.getElementById('user-list');
     userList.innerHTML = `<div class="error">Ocorreu um erro: ${error.message}</div>`;
@@ -31,3 +37,5 @@ function displayPosts(user, title, body, postID) {
     userList.appendChild(post)
 
 };
+
+function displayComments(postID)
