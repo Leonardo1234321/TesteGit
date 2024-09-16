136e29948775c5a1070636c3f6a2304690f8f70e
Leonardo Furtado Kauffmann <leonardo.k@escolar.ifrn.edu.br>
2024-09-16 16:25:18 -0300

    q24, incomplete

---
+++ LISTA01/exercicio24/cadastro_contatos/script_cadas_con.js
@@ -0,0 +1,6 @@
+document.getElementById('cadastro').addEventListener('submit', function(event) {
+  event.preventDefault();  // Evita o comportamento padrão de envio do formulário
+
+
+  console.log('fodase');
+});
