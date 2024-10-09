eae8aa8298e4f03f76e67bde5fa813e10548f4d3
leonardo furtado kauffmann <leonardo.k@escolar.ifrn.edu.br>
2024-10-09 15:11:00 -0300

    q1 resol

--- provaINFOV/q1.js
+++ provaINFOV/q1.js
@@ -1,4 +1,4 @@
-function submit(event) {
+function submit_but(event) {
     event.preventDefault();
     const tel = document.getElementById('tel_num').value;
     const telregex = /\(\d{2}\) \d{5} \d{5}/g;
@@ -8,7 +8,27 @@ function submit(event) {
         document.body.appendChild(result);
         return;
     }
-    result.innerHTML = 'O telefone não está no formato correto';
+    result.innerHTML = 'O telefone não está no formato correto ou está incompleto';
     document.body.appendChild(result);
     return;
 }
+
+function onfocusAutoComplete() {
+    let current_tel = document.getElementById('tel_num').value;
+
+    let occurs = 0;
+    let tel_legth = current_tel.split('').forEach(element => {
+        if (parseInt(element) && parseInt(element) != 0) {
+            occurs ++;
+        };
+        console.log(occurs);
+    })
+    if (current_tel.length === 12) {
+        current_tel = '(' + current_tel.slice(0, 2) + ') ' + current_tel.slice(2, 7) + ' ' + current_tel.slice(7);
+        document.getElementById('tel_num').value = current_tel;
+        return;
+    };
+    return;
+}
+
+document.getElementById('tel_num').addEventListener('focusout', (element) => onfocusAutoComplete(element));
